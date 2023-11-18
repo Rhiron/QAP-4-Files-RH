bc1f83031154d97565e4ab639d7c74f95eff71dd
Rhiron <riley.hiron@keyin.com>
2023-11-18 12:00:25 -0330

    Commiting to add current progress

--- One_Stop_Insurance.py
+++ One_Stop_Insurance.py
@@ -15,7 +15,8 @@ Insurance  premiums  are  calculated  using  a  basic  rate  of  $869.00  for  t
 ***** 
 '''
 # Imports
-
+import datetime
+import Formats as FM
 
 # Constants
 POLICY_NUMBER = 1944
@@ -29,19 +30,32 @@ PROCESSING_FEE = 39.99
 ALLOWED_NAME_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'-"
 ALLOWED_PROV = ["AB", "BC", "MB", "NB", "NL", "NS", "NT", "NU", "ON", "PE", "QC", "SK", "YT"]
 PAYMENT_TYPES = ["Full", "Monthly", "Down Pay"]
+CUR_DATE = datetime.datetime.now()
 
 
 
 # Inputs
 while True:
+    # Creating an empty dictionary to store customer information
+    Customer_Info = {}
+
+    # Gathering Customers First name ending program if blank. Validations(Must be letters, apostrophes, and hyphens)
     while True:
-        Cust_F_Name = input("Enter customers first name: ").title()
+        Cust_F_Name = input("Enter customers first name (Leave Blank to End Program): ").title()
         if set(Cust_F_Name).issubset(ALLOWED_NAME_CHARS):
             break
         elif Cust_F_Name == "":
-            print("First Name cannot be blank - please re-enter")
+            break
         else:
             print("First Name can only contain letters, apostrophes, and hyphens - please re-enter")
+    Customer_Info['First Name'] = Cust_F_Name
+    if Cust_F_Name == "":
+        print('''\n\n========================================================
+||            Program has been terminated             ||
+|| Thank you for using the One Stop Insurance Company ||
+||                 Have a nice day!                   ||
+========================================================\n\n''')
+    # Gathering Customers Last name.  Validations(Must be letters, apostrophes, and hyphens)
     while True:
         Cust_L_Name = input("Enter Customers last name: ").title()
         if set(Cust_L_Name).issubset(ALLOWED_NAME_CHARS):
@@ -50,20 +64,28 @@ while True:
             print("Last Name cannot be blank - please re-enter")
         else:
             print("Last Name can only contain letters, apostrophes, and hyphens - please re-enter")
+    Customer_Info['Last Name'] = Cust_L_Name
     Street_Address = input("Enter Customers street address: ").title()
+    Customer_Info['Street Address'] = Street_Address
     City = input("Enter Customers city: ").title()
+    Customer_Info['City'] = City
+    # Gathering Customers province.  Validations(Must be in the list of allowed provinces)
     while True:
         Prov = input("Enter Customers province (XX): ").upper()
         if Prov in ALLOWED_PROV:
             break
         else:
             print("Province must be one of the following: AB, BC, MB, NB, NL, NS, NT, NU, ON, PE, QC, SK, YT - please re-enter")
+    Customer_Info['Province'] = Prov
+    # Gathering Customers postal code.  Validations(Must be in the format X9X9X9)
     while True:
         Postal_Code = input("Enter Customers postal code (X9X9X9): ").upper()
         if (Postal_Code[::2].isalpha() and Postal_Code[1::2].isdigit()):
             break
         else:
             print("Postal code must be in the format X9X9X9 - please re-enter")
+    Customer_Info['Postal Code'] = Postal_Code
+    # Gathering Customers phone number.  Validations(Must be 10 digits long and must be all digits)
     while True:
         Phone_Num = input("Enter Customers phone number (9999999999): ")
         if Phone_Num.isdigit() and len(Phone_Num) == 10:
@@ -74,7 +96,10 @@ while True:
             print("Phone number must be 10 digits long - please re-enter")
         else:
             print("Phone number can only contain digits - please re-enter")
+    Customer_Info['Phone Number'] = FM.Format_Phone_Num(Phone_Num)
+    # Gathering Number of cars being insured.  Validations(Must be greater than 0)
     while True:
+        # Using try/except to catch invalid inputs
         try:
             Num_Of_Cars = int(input("Enter the number of cars that are being insured: "))
             if Num_Of_Cars > 0:
@@ -83,23 +108,68 @@ while True:
                 print("Number of cars must be greater than 0 - please re-enter")
         except ValueError:
             print("Invalid input - please enter a valid number for the number of cars")
-    
-    Extra_Liability = input("Does the customer want extra liability coverage? (Y/N): ")
-    Glass_Coverage = input("Does the customer want glass coverage? (Y/N): ")
-    Loaner_Car = input("Does the customer want loaner car coverage? (Y/N): ")
-    Payment_Method = input("How is the customer paying? (Full/Monthly/Down Pay): ")
-    if Payment_Method == "Down Pay":
-        Down_Payment = float(input("Enter the amount of the down payment: "))
-    else:
-        Down_Payment = 0.00
-    Claims = []
+    Customer_Info['Number of Cars'] = Num_Of_Cars
+    # Checking if the customer wants extra liability coverage.  Validations(Must be Y or N)
     while True:
-        Claim = input("Enter a claim or press Enter to finish: ")
-        if Claim == "":
+        Extra_Liability = input("Does the customer want extra liability coverage? (Y/N): ").upper()
+        if Extra_Liability == "Y" or Extra_Liability == "N":
             break
         else:
-            Claims.append(Claim)
-    break
+            print("Invalid input - please enter Y or N")
+    Customer_Info['Extra Liability Coverage'] = Extra_Liability
+    # Checking if the customer wants glass coverage.  Validations(Must be Y or N)
+    while True:
+        Glass_Coverage = input("Does the customer want glass coverage? (Y/N): ").upper()
+        if Glass_Coverage == "Y" or Glass_Coverage == "N":
+            break
+        else:
+            print("Invalid input - please enter Y or N")
+    Customer_Info['Glass Coverage'] = Glass_Coverage
+    # Checking if the customer wants loaner car coverage.  Validations(Must be Y or N)
+    while True:
+        Loaner_Car = input("Does the customer want loaner car coverage? (Y/N): ").upper()
+        if Loaner_Car == "Y" or Loaner_Car == "N":
+            break
+        else:
+            print("Invalid input - please enter Y or N")
+    Customer_Info['Loaner Car Coverage'] = Loaner_Car
+    # Checking how the customer wants to pay.  Validations(Must be Full, Monthly, or Down Pay)
+    while True:
+        Payment_Method = input("How is the customer paying? (Full/Monthly/Down Pay): ").title()
+        if Payment_Method in PAYMENT_TYPES:
+            # If Customer wants down payment then asking for the amount.  Validations(Must be greater than 0)
+            if Payment_Method == PAYMENT_TYPES[2]:
+                while True:
+                    # Using try/except to catch invalid inputs
+                    try:
+                        Down_Payment = float(input("Enter the amount of the down payment: "))
+                        if Down_Payment > 0:
+                            break
+                        else:
+                            print("Down payment must be greater than 0 - please re-enter")
+                    except ValueError:
+                        print("Invalid input - please enter a valid number for the down payment")
+            break
+        else:
+            print("Invalid input - please enter Full, Monthly, or Down Pay")
+    Customer_Info['Payment Method'] = Payment_Method
+    # Gathering the Number of previous claims.  Validations(Must be greater than or equal to 0)
+    while True:
+        try:
+            Num_Of_Claims = int(input("Enter the number of previous claims the customer has made: "))
+            if Num_Of_Claims >= 0:
+                break
+            else:
+                print("Number of claims must be greater than or equal to 0 - please re-enter")
+        except ValueError:
+            print("Invalid input - please enter a valid number for the number of claims")
+    Customer_Info['Number of Claims'] = Num_Of_Claims
+    # Creating empty lists to store the previous claims information
+    Claim_Costs = []
+
+    # Print out the customer information
+    print(Customer_Info)
+    
 
 
 # Calculations
